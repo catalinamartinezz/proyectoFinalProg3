ad028954df5dd12a6780e197732eaf206e639009
jacinta <jvillanueva@udesa.edu.ar>
2022-11-03 09:13:22 -0300

    home

---
+++ src/components/HomeMenu.js
@@ -0,0 +1,17 @@
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeMenu() {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Home" component={Home} />
+			<Tab.Screen name="Profile" component={Profile} />
+		</Tab.Navigator>
+	);
+}
+
+export default HomeMenu;
