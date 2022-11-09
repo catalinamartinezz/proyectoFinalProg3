919fb5da1783ee4f319c14e2a7dd271310ae3a4e
luisigarciia <garcial@udesa.edu.ar>
2022-11-09 20:03:31 -0300

    avances perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import { Text, View } from 'react-native-web';
+import {auth,db} from '../firebase/config';
+
 
 export default class Profile extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      users:[],
+    };
+  }
+  componentDidMount(){
+    db.collection("users")
+    .where('email', '==', auth.currentUser.email)
+    .onSnapshot((docs)=>{
+      let usersFromDb=[];
+      docs.forEach((doc)=>{
+        usersFromDb.push({
+          email: doc.email,
+          nombre: doc.nombreDeUsuario(),
+        });
+      });
+      this.setState({users: usersFromDb});
+    });
+  }
   render() {
     return (
-      <div>Profile</div>
+      <View>
+        <Text>{auth.currentUser.nombreDeUsuario}</Text>
+        <Text>{auth.currentUser.email}</Text>
+      </View>
     )
   }
 }
