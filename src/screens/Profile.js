c65403ca4f352b297f8122637a3ca6cefbdb504d
luisigarciia <garcial@udesa.edu.ar>
2022-11-10 09:28:34 -0300

    profile datos

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -7,29 +7,41 @@ export default class Profile extends Component {
   constructor(props){
     super(props);
     this.state={
-      users:[],
+      user:{},
+      loading:true,
     };
   }
   componentDidMount(){
-    db.collection("users")
+    db.collection('users')
     .where('email', '==', auth.currentUser.email)
     .onSnapshot((docs)=>{
-      let usersFromDb=[];
+      let usersFromDb={};
       docs.forEach((doc)=>{
-        usersFromDb.push({
-          email: doc.email,
-          nombre: doc.nombreDeUsuario(),
-        });
+        let user=doc.data();
+        console.log(user);
+        usersFromDb={
+          id:doc.id,
+          data:user,
+        };
+        console.log(usersFromDb);
       });
-      this.setState({users: usersFromDb});
+      this.setState({user: usersFromDb, loading:false});
     });
   }
   render() {
+
     return (
-      <View>
-        <Text>{auth.currentUser.nombreDeUsuario}</Text>
-        <Text>{auth.currentUser.email}</Text>
-      </View>
+      <>
+      {this.state.loading?<Text>Cargando</Text>:<View>
+      <Text>{this.state.user.data.nombreUsuario}</Text>
+      <Text>{this.state.user.data.email}</Text>
+      <Text>{this.state.user.data.miniBio}</Text>
+    </View>}
+      </>
     )
   }
 }
+const styles={
+  
+}
+
