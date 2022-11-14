095157d94ff60f5cde672e4f2e981eb89801dc8a
jacinta <jvillanueva@udesa.edu.ar>
2022-11-14 11:11:45 -0300

    buscador

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -3,9 +3,10 @@ import Profile from "../screens/Profile";
 import NewPost from "../screens/NewPost";
 import React, { Component } from 'react';
 
-import {Foundation, Ionicons} from '@expo/vector-icons'
+import {Foundation, Ionicons, AntDesign} from '@expo/vector-icons'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Search from "../screens/Search";
 
 const Tab = createBottomTabNavigator();
 
@@ -31,6 +32,11 @@ export default class HomeMenu extends Component {
 				name="NewPost" 
 				component={NewPost} 
 				options={{ tabBarIcon: () => <Foundation name="photo" size={24} color="#98A8F8" /> }}
+			/>
+			<Tab.Screen 
+				name="Search" 
+				component={Search} 
+				options={{ tabBarIcon: () => <AntDesign name="search1" size={24} color="#98A8F8" />}}
 			/>
 		</Tab.Navigator>
 	);
